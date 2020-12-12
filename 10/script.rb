07eb4c4341dc9df35ed7ab8cbe3abd855d67848c
Andrea Mazzini <andrea.mazzini@gmail.com>
2020-12-12 12:00:16 +0100

    Improve day 10

--- 10/script.rb
+++ 10/script.rb
@@ -2,11 +2,9 @@ numbers = [0]
 numbers += File.open('data.txt', 'r').map{ |l| l.strip.to_i }
 numbers += [numbers.max + 3]
 differences = numbers.sort.each_cons(2).map { |pair| pair[1] - pair[0] }
-d1 = differences.filter { |n| n == 1 }.size
-d3 = differences.filter { |n| n == 3 }.size
 
 puts '# Day 10 - Part 1:'
-puts d1 * d3
+puts [1, 3].map { |distance| differences.filter { |n| n == distance }.size }.reduce(&:*)
 
 puts '# Day 10 - Part 2:'
 puts differences.chunk { |n| n == 1 }.map { |is_one, ary| is_one ? (ary.size * (ary.size - 1)) / 2 + 1 : 1 }.reduce(&:*)
